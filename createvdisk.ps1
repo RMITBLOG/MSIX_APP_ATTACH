5733fc991f30cd7d3b75d76e3bb6cc3aa12b88df
RMITBLOG <48697709+RMITBLOG@users.noreply.github.com>
2020-08-19 23:45:24 +0100

    Create createvdisk.ps1

---
+++ createvdisk.ps1
@@ -0,0 +1,25 @@
+<#	
+	.NOTES
+	===========================================================================
+	 
+	 Created on:   	19/08/2020 23:38
+	 Created by:   	Ryan Mangan
+	 Organization: 	ryanmangans IT BLOG LTD
+	 Filename:     	createvdisk.ps1
+	===========================================================================
+	.DESCRIPTION
+		Create a virtual disk for MSIX App Attach
+#>
+
+# Create Vdisk for App Attach
+$filepath = read-Host "Wnter the file path and name for the VHD e.g c:\temp\notepad++.vhd"
+#Create vdisk
+New-VHD -SizeBytes 1024MB -Path $filepath -Dynamic -Confirm:$false
+#mount vdisk
+$vhdObject = Mount-VHD $filepath -Passthru
+#Intialise the disk
+$vdisk = Initialize-Disk -Passthru -Number $vhdObject.Number
+# create a disk partition 
+$partition = New-Partition -AssignDriveLetter -UseMaximumSize -DiskNumber $vdisk.Number
+# Format-Volume -FileSystem NTFS -Confirm:$false -DriveLetter $partition.DriveLetter -Force
+Format-Volume -FileSystem NTFS -Confirm:$false -DriveLetter $partition.DriveLetter -Force
