6508f105f569e9be01bec2c07024824e668e3104
RMITBLOG <48697709+RMITBLOG@users.noreply.github.com>
2021-02-11 02:17:43 +0000

    Create list-cims.ps1

---
+++ Cim Scripts/list-cims.ps1
@@ -0,0 +1,14 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	10/02/2021 19:07
+	 Created by:   	Ryan Mangan
+	 Organization: 	Ryanmangansitblog ltd
+	 Filename:     	list-cims.ps1
+	===========================================================================
+	.DESCRIPTION
+		script to list only CIMFS mounted images
+#>
+
+
+Get-ciminstance -classname win32_volume | where filesystem -EQ 'cimfs' | select name, filesystem, DeviceID
