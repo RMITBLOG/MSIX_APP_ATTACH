0773fb523e9a63e9ce5506b4113edfc7fbc64da1
Ryan Mangan <48697709+RMITBLOG@users.noreply.github.com>
2021-05-07 01:17:50 +0100

    Deployment script for MSIX Log Explorer

---
+++ MSIX Event Log tool/MSIX Log Explorer Deploy.ps1
@@ -0,0 +1,55 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	07/05/2021 00:41
+	 Created by:   	Ryan Mangan	
+	 Organization: 	Ryan Mangans IT Blog
+	 Filename:     	MSIX Log Explorer WVD Host Installation Script.
+	===========================================================================
+	.DESCRIPTION
+		This script downloads MSIX Log Explorer from the RMITBLOG GIT Hub Repo and installed on the required WVD Session host.
+
+		The Script has been designed to be used with Azure Script Extentions to automate the process of deploying the MSIX Log Explorer on session hosts.
+
+		Any issues, please feedback through GitHUB.
+#>
+
+
+
+
+
+#Run app on user logon.
+
+New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run -Name MSIXLOGEXPLORER -Value "C:\Program Files\Ryanmangansitblog ltd\MSIX Log Explorer\MSIX log Explorer.exe"
+
+
+
+
+# Define where to store logs
+[string]$temPAth = 'C:\temp\'
+
+# Create folder if does not exist
+if (!(Test-Path -Path $temPAth))
+{
+	$paramNewItem = @{
+		Path	 = $temPAth
+		ItemType = 'Directory'
+		Force    = $true
+	}
+	
+	New-Item @paramNewItem
+}
+
+
+#Download MSIX Log Explorer
+invoke-WebRequest -Uri "https://github.com/RMITBLOG/MSIX_APP_ATTACH/blob/master/MSIX%20Event%20Log%20tool/MSIX%20Log%20Explorer.msi?raw=true" -OutFile "C:\temp\MSIXLOGEXPLORER.msi"
+Start-Sleep -s 5
+
+
+#Install MSIX Log Explorer
+msiexec /i "C:\temp\MSIXLOGEXPLORER.msi" /q /n
+Start-Sleep -s 2
+
+Write-Host "Installation complete"
+
+ 
